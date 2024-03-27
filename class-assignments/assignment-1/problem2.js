2e70ae04312a9aa8d84ce286f16543607e22d3cb
Vishal9956 <114722671+Vishal9956@users.noreply.github.com>
2024-03-28 02:17:55 +0530

    problem 2 completed

--- class-assignments/assignment-1/problem2.js
+++ class-assignments/assignment-1/problem2.js
@@ -1,1 +1,17 @@
-const name = 'vishal';
+let num1;
+let num2;
+let result;
+function utilityFuntion(){
+    num1=document.querySelector("#num1").value;
+    num2=document.querySelector("#num2").value;
+}
+function multiplyNumbers(){
+    utilityFuntion();
+    result=num1*num2;
+    document.querySelector("#result").textContent=result;
+}
+function divideNumbers(){
+    utilityFuntion();
+    result=num1/num2;
+    document.querySelector("#result").textContent=result;
+}
